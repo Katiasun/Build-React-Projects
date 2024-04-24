c314826cf35dd064784185d5a9c0067e75a91f58
Katiasun <katjatolok3@gmail.com>
2024-04-24 17:42:10 +0200

    use useEffect

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ImageSlider({ url, limit }) {
   const [images, setImages] = useState([]);
   const [currentSlider, setCurrentSlider] = useState(0);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    async function fetchImages(getUrl) {
+        try {
+            setLoading(true)
+
+            const response = await fetch(getUrl);
+            const data = await response.json();
+
+            if (data) {
+                setImages(data)
+                setLoading(false)
+            }
+        } catch (e) {
+            setErrorMsg(e.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (url !== "") {
+            fetchImages(url);
+        }
+    }, [url])
+
+    return()
 }
