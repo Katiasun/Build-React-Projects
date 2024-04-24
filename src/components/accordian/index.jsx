4ac4b79aa49f9a1ce71294dc7f8388786576ba31
Katiasun <katjatolok3@gmail.com>
2024-04-24 12:22:01 +0200

    -

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -7,7 +7,7 @@ export default function Accordian() {
   const [selected, setSelected] = useState(null);
 
   function handleSingleSelection(getCurrentId) {
-    console.log(getCurrentId);
+    setSelected(getCurrentId === selected ? null : getCurrentId);
   }
 
   return (
@@ -16,10 +16,11 @@ export default function Accordian() {
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
-              <div onClick={() => handleSingleSelection(data.id)} className="title">
+              <div onClick={() => handleSingleSelection(dataItem.id)} className="title">
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? <div className="content">{dataItem.answer}</div> : null}
             </div>
           ))
         ) : (
