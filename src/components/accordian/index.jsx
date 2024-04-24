dc466681204ecdac1f906af93326e8b0af6d4f3c
Katiasun <katjatolok3@gmail.com>
2024-04-24 12:06:50 +0200

    write code inside method map, display single elemet

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,12 +1,27 @@
 //accordian selection
 // multiple selection
 import { useState } from "react";
+import data from "./data";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
+
   return (
     <div className="wrapper">
-      <div className="accordion"></div>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
     </div>
   );
 }
